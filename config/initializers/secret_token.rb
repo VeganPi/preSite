ef130b66b3b74676e5565414d168ba3132d072ac
Emily Durrant <emily.l.durrant@gmail.com>
2013-04-22 01:11:29 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PreSite::Application.config.secret_token = '94ecefef0b037644b56df34e57ef661f2b7a6426d24f209fc393e2dd40a883420559ea38b4940af845bb849e911ba2279b8e46f1f37984ffd6f9b09b7bde311b'
